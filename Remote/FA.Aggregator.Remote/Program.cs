dce1e6ecf3a41a6051dcbb1824af6261f4f1bb09
Olt Egor <e.olt@softpro.com>
2019-08-11 15:01:31 +0300

    remote

--- Remote/FA.Aggregator.Remote/Program.cs
+++ Remote/FA.Aggregator.Remote/Program.cs
@@ -4,18 +4,18 @@ using FA.Common.Messages;
 using FA.utils;
 using System;
 
-namespace FA.Aggregator
+namespace FA.Aggregator.Remote
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var faActorSystem = ActorSystem.Create("faActorSystem", HoconLoader.ParseConfig("fa.aggregator.hocon"));
-            
+            var faActorSystem = ActorSystem.Create("AggregatorSystem", HoconLoader.ParseConfig("fa.aggregator.remote.hocon"));
+
             var asian = faActorSystem.ActorOf<ScanerActor>("scaner_asian");
             var greenFeed = faActorSystem.ActorOf<ScanerActor>("scaner_greenFeed");
             var aggregator = faActorSystem.ActorOf<AggregatorActor>("aggregator");
-           
+
             var scaners = faActorSystem.ActorSelection("/user/scaner*");
 
             faActorSystem
@@ -25,8 +25,10 @@ namespace FA.Aggregator
                 scaners, new ScanCommand(), ActorRefs.NoSender);
 
 
-            Console.WriteLine("Press any key.");
-            Console.ReadKey();
+            
+
+            faActorSystem.WhenTerminated.Wait();
+            
         }
     }
 }
---
+++ Remote/FA.Scaner.Remote/Program.cs
@@ -0,0 +1,14 @@
+﻿using Akka.Actor;
+using FA.utils;
+
+namespace FA.Scaner.Remote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var actorSystem = ActorSystem.Create("ScanerSystem", HoconLoader.ParseConfig("fa.scaner.remote.hocon"));
+            actorSystem.WhenTerminated.Wait();
+        }
+    }
+}
